7b42c8c6e2424dc1077e11a56d1a800c67bc616a
Bartosz Borne <b.borne@outlook.com>
2023-08-02 17:43:32 +0100

    Reworked output formatting + tick interval to 1s

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # offtimer.py
 
-A Python command-line utility for Windows that lets you countdown to a shut down, restart, hibernate or log off.
+A Python command-line utility for Windows that lets you countdown to a shut down, restart, hibernate, log off or sleep[^1].
+[^1]: The sleep option uses the Win+X menu found on Windows 10 and 11, and the 'keyboard' python module.
 
 ### Usage
 
-In the command line (inside the file's directory), type:\
+In the command line (inside the file's directory), type:
 `python .\offtimer.py <behaviour> <minutes>`
 
 Behaviour argument must be specified (there is no default):
@@ -16,12 +17,12 @@ Behaviour argument must be specified (there is no default):
 | `l` | Log off |
 | `z` | Sleep (requires 'keyboard' module) |
 
-If no time argument is provided, the action will be taken immediately.\
+If no time argument is provided, the action will be taken immediately.
 Press <kbd>Ctrl</kbd>+<kbd>C</kbd> to abort before the timer runs down.
 
 ### Example
 
 To hibernate in 25 minutes, type `python .\offtimer.py h 25`.
-The utility will show a countdown of the time remaining in 5 second decrements, then execute the behaviour specified.
+The utility will show a countdown of the time remaining, then execute the behaviour specified.
 
-![utility counting down to hibernate inside Windows Terminal](example_image.png)
+![Utility counting down to hibernate inside Windows Terminal](example_image.png)
--- offtimer.py
+++ offtimer.py
@@ -2,12 +2,16 @@ import sys
 import os
 from time import sleep
 
-options = {
-    's': "Shutting down",
-    'r': "Restarting",
-    'h': "Hibernating",
-    'l': "Logging off",
-    'z': "Sleeping"
+LINE_UP = '\033[1A'
+LINE_CLEAR = '\x1b[2K'
+TIME_TICKER_INTERVAL_SECONDS = 1
+
+OPTIONS = {
+    's': "SHUT DOWN",
+    'r': "RESTART",
+    'h': "HIBERNATE",
+    'l': "LOG OFF",
+    'z': "SLEEP"
 }
 
 def countdown(opt, behav, mins) -> None:
@@ -21,19 +25,23 @@ def countdown(opt, behav, mins) -> None:
     mins_left = mins
     secs_left = 0
     
-    print(f"Press Ctrl-C to Cancel. {behav} in:\n{mins_left}:{secs_left:02d} ", end='')
-    sleep(5)
+    print(f"{behav} IN {mins} MINUTES countdown started. Press Ctrl-C to Cancel.")
+    print(f"Time remaining: {mins_left}:{secs_left:02d}")
+    sleep(TIME_TICKER_INTERVAL_SECONDS)
     mins_left -= 1
 
     while mins_left >= 0:
-        secs_left = 55
+        secs_left = 60 - TIME_TICKER_INTERVAL_SECONDS
 
         while secs_left >= 0:
-            print(f"\r{mins_left}:{secs_left:02d} ", end='')
-            secs_left -= 5
-            sleep(5)
+            print(LINE_UP, end=LINE_CLEAR)
+            print(f"Time remaining: {mins_left}:{secs_left:02d}")
+            secs_left -= TIME_TICKER_INTERVAL_SECONDS
+            sleep(TIME_TICKER_INTERVAL_SECONDS)
 
         mins_left -= 1
+    
+    print(f"=={behav}==")
 
     if opt == 'z':
         kb.send("win+x")
@@ -47,7 +55,7 @@ def countdown(opt, behav, mins) -> None:
 def main() -> int:
     try:
         opt_arg = sys.argv[1]
-        behav_text = options[opt_arg]
+        behav_text = OPTIONS[opt_arg]
 
         try:
             mins_arg = int(sys.argv[2]) if len(sys.argv) > 2 else 0
@@ -56,9 +64,14 @@ def main() -> int:
         else:
             countdown(opt_arg, behav_text, mins_arg)
     except KeyboardInterrupt:
-        print("\tAbort\n")
+        print("==Aborted==")
     except (IndexError, KeyError):
-        print("No or invalid behaviour argument. Accepted arguments:\n s : shutdown\n r : restart\n h : hibernate\n l : log off\n z : sleep (requires keyboard module)\n")
+        print("No or invalid behaviour argument. Accepted arguments:\n" \
+              " s : shutdown\n" \
+              " r : restart\n" \
+              " h : hibernate\n" \
+              " l : log off\n" \
+              " z : sleep (requires 'keyboard' module)\n")
     
     return 0
 
